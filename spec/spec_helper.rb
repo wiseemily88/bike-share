66c62eb2d7c4d4bfb5535654b4a6e0d6979dac3f
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-03 16:07:00 -0600

    add station table and database-cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,14 @@ Capybara.app = BikeShareApp
 RSpec.configure do |c|
   c.include Capybara::DSL
 end
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.configure do |c|
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
