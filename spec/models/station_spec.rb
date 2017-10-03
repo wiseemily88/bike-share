073fdbb98f32d44708f89630bd687c38e60b3f8d
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-03 17:14:56 -0600

    add more validation tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,15 +1,42 @@
 describe Station do
-  describe "Class Methods" do
-    describe "method" do
-      it ""
-    end
-  end
 
   describe "Validation" do
     it "is invalid without a name" do
-      station = Station.new(dock_count: 1, city: "Denver", installation_date: 10/3/2017)
+      station = Station.new(dock_count: 1, city: "Denver", installation_date: Date.new(2015, 12, 8))
+
+      expect(station).to_not be_valid
+    end
+
+    it "is invalid without a dock_count" do
+      station = Station.new(name: "Luis and 3/4", city: "Denver", installation_date: Date.new(2015, 12, 8))
+
+      expect(station).to_not be_valid
+    end
+
+    it "is invalid without a city" do
+      station = Station.new(name: "Luis and 3/4", dock_count: 1, installation_date: Date.new(2015, 12, 8))
+
+      expect(station).to_not be_valid
+    end
+
+    it "is invalid without a installation_date" do
+      station = Station.new(name: "Luis and 3/4", dock_count: 1, city: "Denver")
+
+      expect(station).to_not be_valid
+    end
+
+    it "is invalid if name is not unique" do
+      Station.create(name: "Luis and 3/4", dock_count: 1, city: "Denver", installation_date: Date.new(2015, 12, 8))
+      station = Station.create(name: "Luis and 3/4", dock_count: 2, city: "Kansas", installation_date: Date.new(2015, 12, 9))
 
       expect(station).to_not be_valid
     end
   end
+
+  # describe "Class Methods" do
+  #   describe "method" do
+  #     it ""
+  #   end
+  # end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ Bundler.require(:default, :test)
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'Date'
 
 Capybara.app = BikeShareApp
 
