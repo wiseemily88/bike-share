e8b919b38e73ed7b1c67dd280f5dacb2dee37176
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-03 16:16:27 -0600

    add validations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,9 @@
 class Station < ActiveRecord::Base
 
-
+  # Dedicated to Sal...
+  validates :name,              presence: true, uniquness: true
+  validates :dock_count,        presence: true
+  validates :city,              presence: true
+  validates :installation_date, presence: true
 
 end
