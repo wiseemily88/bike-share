c31b627212fabd4f1e5171414670e5ef0e637d25
Emily Wise <wise.m.emily@gmail.com>
2017-10-04 14:49:09 -0600

    Add New View

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,7 @@
 class Station < ActiveRecord::Base
 
-  # Dedicated to Sal...
   validates :name,              presence: true, uniqueness: true
   validates :dock_count,        presence: true
   validates :city,              presence: true
   validates :installation_date, presence: true
-
 end
--- db/migrate/20171003220004_create_stations.rb
+++ db/migrate/20171003220004_create_stations.rb
@@ -10,15 +10,3 @@ class CreateStations < ActiveRecord::Migration[5.1]
     end
   end
 end
-
-# class CreateFilms < ActiveRecord::Migration
-#   def change
-#     create_table :films do |t|
-#       t.text    :title
-#       t.integer :year
-#       t.integer :box_office_sales
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
