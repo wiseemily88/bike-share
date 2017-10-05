aad51bf7f09dc4362aa2a7278931fda7553e2bd5
Dione Wilson <dionew1@gmail.com>
2017-10-05 15:46:04 -0600

    Iteration2

---
+++ app/models/trip.rb
@@ -0,0 +1,18 @@
+class Trips < ActiveRecord::Migration[5.1]
+  def change
+    create_table :trips do |t|
+      t.integer  :duration
+      t.date     :start_date
+      t.string   :start_station_name
+      t.integer  :start_station_id
+      t.date     :end_date
+      t.string   :end_station_name
+      t.integer  :end_station_id
+      t.integer  :bike_id
+      t.string   :subscription_type
+      t.integer  :zip_code
+
+      t.timestamps null: false
+    end
+  end
+end
