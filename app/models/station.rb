632d7aff86c344ca46cd303606ff579286401f7a
Emily Wise <wise.m.emily@gmail.com>
2017-10-07 14:44:42 -0600

    moved logic to Station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,4 +6,25 @@ class Station < ActiveRecord::Base
   validates :installation_date, presence: true
   validates :lat,               presence: true
   validates :long,              presence: true
+
+  def self.average_bikes_per_stations
+    average("dock_count").floor
+  end
+
+  def self.highest_bike_count
+    maximum("dock_count")
+  end
+
+  def self.lowest_bike_count
+    minimum("dock_count")
+  end
+
+  def self.newest_station
+    maximum("installation_date")
+  end
+
+  def self.oldest_station
+    minimum("installation_date")
+  end
+
 end
