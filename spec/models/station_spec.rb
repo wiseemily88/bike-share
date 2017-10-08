c94ab6e0139883fcfd42474ff4acf34c9e8343cd
Emily Wise <wise.m.emily@gmail.com>
2017-10-08 15:48:08 -0600

    Fixed bugs

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Station do
   describe ".count" do
     it "returns total number of stations" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
       expect(Station.count).to eq(2)
     end
@@ -46,7 +46,7 @@ RSpec.describe Station do
   describe ".average_bikes_per_stations" do
     it "returns average bikes per station" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
       expect(Station.average_bikes_per_stations).to eq(14)
     end
@@ -55,7 +55,7 @@ RSpec.describe Station do
   describe ".highest_bike_count" do
     it "returns bike count value" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
       expect(Station.highest_bike_count).to eq(15)
     end
@@ -64,7 +64,7 @@ RSpec.describe Station do
   describe ".lowest_bike_count" do
     it "returns bike count value" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
       expect(Station.lowest_bike_count).to eq(13)
     end
@@ -73,18 +73,18 @@ RSpec.describe Station do
   describe ".newest_station" do
     it "returns the date of the newest station" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
-      expect(Station.newest_station).to eq(Date.new(2015, 15, 8))
+      expect(Station.newest_station).to eq(Date.new(2015, 12, 8))
     end
   end
 
   describe ".oldest_station" do
     it "returns the date of the oldest station" do
       Station.create(name: "Station 2", dock_count: 13, city: "Denver", installation_date: Date.new(2015, 12, 8),long: 2.2, lat: 3.3)
-      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 15, 8),long: 2.2, lat: 3.3)
+      Station.create(name: "Station 1", dock_count: 15, city: "Boston", installation_date: Date.new(2015, 10, 8),long: 2.2, lat: 3.3)
 
-      expect(Station.oldest_station).to eq(Date.new(2015, 12, 8))
+      expect(Station.oldest_station).to eq(Date.new(2015, 10, 8))
     end
   end
 
