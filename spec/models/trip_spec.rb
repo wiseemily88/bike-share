f87e69a421099898d2c601ec3370ec72cd7ab077
Emily Wise <wise.m.emily@gmail.com>
2017-10-07 20:20:38 -0600

    Add validation tests

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,115 @@
+require_relative '../spec_helper'
+RSpec.describe Trip do
+
+  describe "Validation" do
+    it "is invalid without a duration" do
+      trip = Trip.new(
+         start_date: 8/29/2013 14:13,
+         start_station_name: "South Van Ness at Market",
+         start_station_id: 66,
+         end_date: 8/29/2013 14:14,
+         end_station_name: "South Van Ness at Market",
+         end_station_id: 66,
+         bike_id: 520,
+         subscription_type: "Subscriber",
+         zip_code: 94127)
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a start_date" do
+      trip = Trip.new(
+        duration: 63,
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: 8/29/2013 14:14,
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 520,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a start_station_name" do
+      trip = Trip.new(
+        duration: 63,
+        start_date: 8/29/2013 14:13,
+        start_station_id: 66,
+        end_date: 8/29/2013 14:14,
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 520,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a end_date" do
+      trip = Trip.new(
+        duration: 63,
+        start_date: 8/29/2013 14:13,
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 520,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a end_station_name" do
+      trip = Trip.new(
+        duration: 63,
+        start_date: 8/29/2013 14:13,
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: 8/29/2013 14:14,
+        end_station_id: 66,
+        bike_id: 520,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a bike_id" do
+      trip = Trip.new(
+        duration: 63,
+        start_date: 8/29/2013 14:13,
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: 8/29/2013 14:14,
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is invalid without a subscription_type" do
+      trip = Trip.new(
+        duration: 63,
+        start_date: 8/29/2013 14:13,
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: 8/29/2013 14:14,
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 520,
+        zip_code: 94127
+       )
+
+      expect(trip).to_not be_valid
+    end
+  end
