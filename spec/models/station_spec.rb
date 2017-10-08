2d56d9bc70db0862af1d11fc9db129d4b5a6e12f
Dione Wilson <dionew1@gmail.com>
2017-10-08 09:46:45 -0600

    WIP tests and views

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -4,33 +4,45 @@ describe Station do
 
   describe "Validation" do
     it "is invalid without a name" do
-      station = Station.new(dock_count: 1, city: "Denver", installation_date: Date.new(2015, 12, 8))
+      station = Station.new(lat: 37.329732, long: -121.901782, dock_count: 1,
+                            city: "Denver",
+                            installation_date: Date.new(2015, 12, 8))
 
       expect(station).to_not be_valid
     end
 
     it "is invalid without a dock_count" do
-      station = Station.new(name: "Luis and 3/4", city: "Denver", installation_date: Date.new(2015, 12, 8))
+      station = Station.new(name: "Luis and 3/4", lat: 37.329732,
+                            long: -121.901782, city: "Denver",
+                            installation_date: Date.new(2015, 12, 8))
 
       expect(station).to_not be_valid
     end
 
     it "is invalid without a city" do
-      station = Station.new(name: "Luis and 3/4", dock_count: 1, installation_date: Date.new(2015, 12, 8))
+      station = Station.new(name: "Luis and 3/4", lat: 37.329732,
+                            long: -121.901782, dock_count: 1,
+                            installation_date: Date.new(2015, 12, 8))
 
       expect(station).to_not be_valid
     end
 
     it "is invalid without a installation_date" do
-      station = Station.new(name: "Luis and 3/4", dock_count: 1, city: "Denver")
+      station = Station.new(name: "Luis and 3/4", lat: 37.329732,
+                            long: -121.901782, dock_count: 1, city: "Denver")
 
       expect(station).to_not be_valid
     end
 
     it "is invalid if name is not unique" do
-      Station.create(name: "Luis and 3/4", dock_count: 1, city: "Denver", installation_date: Date.new(2015, 12, 8))
-      station = Station.create(name: "Luis and 3/4", dock_count: 2, city: "Kansas", installation_date: Date.new(2015, 12, 9))
-
+      Station.create(name: "Luis and 3/4", lat: 37.329732, long: -121.901782,
+                     dock_count: 1, city: "Denver",
+                     installation_date: Date.new(2015, 12, 8))
+      station = Station.create(name: "Luis and 3/4",
+                               lat: 37.329732,
+                               long: -121.901782,
+                               dock_count: 2, city: "Kansas",
+                               installation_date: Date.new(2015, 12, 9))
       expect(station).to_not be_valid
     end
   end
