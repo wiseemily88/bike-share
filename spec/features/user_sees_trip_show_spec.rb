3ea1068775b515768fbcb6c2a6d88f906bdceff1
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-10 15:25:37 -0600

    add feature tests for trips

--- spec/features/user_sees_trip_show_spec.rb
+++ spec/features/user_sees_trip_show_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
-RSpec.describe "When a user visits '/stations/:id'" do
+RSpec.describe "When a user visits '/trips/:id'" do
 
-  let(:station) do
+  let(:trip) do
     Trip.create!(
       duration: 63,
       start_date: DateTime.new(2013,8,28,14,14),
@@ -18,18 +18,38 @@ RSpec.describe "When a user visits '/stations/:id'" do
   end
 
   before do
-    visit "/stations/#{station.id}"
+    visit "/trips/#{trip.id}"
   end
 
-  it "they see a station name" do
-    expect(page).to have_content("Miami Bike")
+  it "they see a duration" do
+    expect(page).to have_content("Duration: 63")
   end
 
-  it "they see dock count" do
-    expect(page).to have_content("Dock Count: #{station.dock_count}")
+  it "they see start date" do
+    expect(page).to have_content("Start Date: #{trip.start_date}")
   end
 
-  it "they see city" do
-    expect(page).to have_content("City: #{station.city}")
+  it "they see start station name" do
+    expect(page).to have_content("Start Station Name: Station 1")
+  end
+
+  it "they see end date" do
+    expect(page).to have_content("End Date: #{trip.end_date}")
+  end
+
+  it "they see end station name" do
+    expect(page).to have_content("End Station Name: Station 2")
+  end
+
+  it "they see bike id" do
+    expect(page).to have_content("Bike ID: 520")
+  end
+
+  it "they see subscription type" do
+    expect(page).to have_content("Subscription Type: Subscriber")
+  end
+
+  it "they see zip code" do
+    expect(page).to have_content("Zip Code: 94127")
   end
 end
