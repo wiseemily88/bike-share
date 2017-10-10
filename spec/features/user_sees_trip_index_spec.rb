ae6f61a55a991d768ff62f4a599b7c4d9896954e
Dione Wilson <dionew1@gmail.com>
2017-10-10 10:08:30 -0600

    Update test

---
+++ spec/features/user_sees_trip_index_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Feature" do
+  describe "when a user visits '/trips'" do
+    it "they see trips" do
+      visit "/tripss"
+
+      within("#body-content") do
+        expect(page).to have_content("Trips Taken")
+      end
+    end
+  end
+
+  describe "when a user clicks on edit" do
+    it "they will see the edit page" do
+>>>>>>
+      visit "/trips"
+      click_on "Edit"
+
+      expect(page).to have_content("Edit Trip")
+    end
+  end
+
+  describe "when a user clicks on create a new trip" do
+    it "they will see the new station page" do
+>>>>>>
+      visit "/trips"
+      click_on "Create a new trip"
+
+      expect(page).to have_content("New Trip")
+    end
+  end
+
+  describe "when a user clicks on create a show" do
+    it "they will see the show page" do
+>>>>>>
+      visit "/trips"
+      click_on "Show"
+
+      expect(page).to have_content()
+    end
+  end
+end
