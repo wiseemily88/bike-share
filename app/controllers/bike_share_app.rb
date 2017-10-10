c1edbb6038beb798fc80b7ab95807b64396b771e
Luis Garcia <garcia@yahoo.com>
2017-10-09 22:10:51 -0600

    CRUD itr7 complete

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -83,4 +83,45 @@ class BikeShareApp < Sinatra::Base
     @trips = Trip.all
     erb :'/trip/dashboard'
   end
+
+  get '/conditions' do
+    @conditions = Condition.all
+    erb :'/condition/index'
+  end
+
+  get '/conditions/new' do
+    erb :'/condition/new'
+  end
+
+  post '/conditions' do
+    @conditions = Condition.create(params[:condition])
+    redirect "/conditions/#{@conditions.id}"
+  end
+
+  get '/conditions/:id' do
+    @conditions = Condition.find(params[:id])
+    erb :'/condition/show'
+  end
+
+  get '/conditions/:id/edit' do
+    @conditions = Condition.find(params[:id])
+    erb :'/condition/edit'
+  end
+
+  put '/conditions/:id' do
+    @condition = Condition.update(params[:condition])
+    redirect "/conditions/#{params[:id]}"
+  end
+
+  delete '/conditions/:id' do
+    Condition.destroy(params[:id])
+    redirect '/conditions'
+  end
+
+  get '/condition-dashboard' do
+    @conditions = Condition.all
+    erb :'/condition/dashboard'
+  end
+
+
 end
