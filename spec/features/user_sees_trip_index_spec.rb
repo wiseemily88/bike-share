7792cd82e7a6e3314e05e9e145ac3720dd0aafaf
Dione Wilson <dionew1@gmail.com>
2017-10-10 10:14:06 -0600

    Finish feature test for trip index

--- spec/features/user_sees_trip_index_spec.rb
+++ spec/features/user_sees_trip_index_spec.rb
@@ -13,7 +13,17 @@ describe "Feature" do
 
   describe "when a user clicks on edit" do
     it "they will see the edit page" do
->>>>>>
+      Trip.create(
+        duration: 63,
+        start_date: DateTime.new(2014,8,29,14,13),
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: DateTime.new(2014,8,29,14,14),
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 400,
+        zip_code: 94127,
+        subscription_type: "Subscriber")
       visit "/trips"
       click_on "Edit"
 
@@ -23,7 +33,17 @@ describe "Feature" do
 
   describe "when a user clicks on create a new trip" do
     it "they will see the new station page" do
->>>>>>
+      Trip.create(
+        duration: 63,
+        start_date: DateTime.new(2014,8,29,14,13),
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: DateTime.new(2014,8,29,14,14),
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 400,
+        zip_code: 94127,
+        subscription_type: "Subscriber")
       visit "/trips"
       click_on "Create a new trip"
 
@@ -33,11 +53,21 @@ describe "Feature" do
 
   describe "when a user clicks on create a show" do
     it "they will see the show page" do
->>>>>>
+      Trip.create(
+        duration: 63,
+        start_date: DateTime.new(2014,8,29,14,13),
+        start_station_name: "South Van Ness at Market",
+        start_station_id: 66,
+        end_date: DateTime.new(2014,8,29,14,14),
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 400,
+        zip_code: 94127,
+        subscription_type: "Subscriber")
       visit "/trips"
       click_on "Show"
 
-      expect(page).to have_content()
+      expect(page).to have_content(Trip)
     end
   end
 end
