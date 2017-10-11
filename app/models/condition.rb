2a8211a715d9fa68520714458f5bb955a42946e4
Dione Wilson <dionew1@gmail.com>
2017-10-11 11:02:38 -0600

    WIP

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,14 +1,19 @@
 class Condition < ActiveRecord::Base
   has_many :trip_conditions , class_name: "Trips", foreign_key: "start_date"
 
-  validates :date,                presence: true, uniqueness: true
-  validates :max_temperature_f,    presence: true
-  validates :min_temperature_f,   presence: true
-  validates :mean_temperature_f,  presence: true
-  validates :mean_humidity,       presence: true
-  validates :mean_visibility_miles,presence: true
-  validates :mean_wind_speed_mph,  presence: true
-  validates :precipitation_inches,presence: true
-  validates :zip_code,            presence: true
+  validates :date,                  presence: true, uniqueness: true
+  validates :max_temperature_f,     presence: true
+  validates :min_temperature_f,     presence: true
+  validates :mean_temperature_f,    presence: true
+  validates :mean_humidity,         presence: true
+  validates :mean_visibility_miles, presence: true
+  validates :mean_wind_speed_mph,   presence: true
+  validates :precipitation_inches,  presence: true
+  validates :zip_code,              presence: true
+
+  def self.mean_trips_for_wind_speeds
+    require 'pry'; binding.pry
+    count(:all, group: "start_date, mean_wind_speed_mph")
+  end
 
 end
