d8a10d496e757d3ee91b8648dde214044c15c504
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-11 11:01:21 -0600

    fix condition-trip relationships

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,9 +1,7 @@
 class Trip < ActiveRecord::Base
   belongs_to :start_station,  class_name: "Station"
   belongs_to :end_station,  class_name: "Station"
-  belongs_to :condition_date,  class_name: "Condition"
-
-
+  belongs_to :condition,  class_name: "Condition", primary_key: "date"
 
   validates :duration,          presence: true
   validates :start_date,        presence: true
