045c425a206975e57480b94a88f0e44a1190493d
Dione Wilson <dionew1@gmail.com>
2017-10-11 14:50:11 -0600

    completed visibility

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -14,7 +14,7 @@ class Condition < ActiveRecord::Base
   validates :zip_code,              presence: true
 
   def self.trips_with_wind_speed(min, max)
-    testing = joins(:trips)
+    joins(:trips)
     .where("mean_wind_speed_mph >= ? AND mean_wind_speed_mph <= ?", min, max)
   end
 
@@ -35,6 +35,35 @@ class Condition < ActiveRecord::Base
 
   def self.min_trips_with_wind_speed(min, max)
     trips = trips_with_wind_speed(min, max)
+    trips.group('start_date')
+    .order('count_all DESC')
+    .count
+    .values
+    .last
+  end
+
+  def self.trips_with_visibility(min, max)
+    joins(:trips)
+    .where("mean_visibility_miles >= ? AND mean_visibility_miles <= ?", min, max)
+  end
+
+  def self.average_trips_with_visibility(min, max)
+    trips = trips_with_visibility(min, max).count
+    total_days = where("mean_visibility_miles >= ? AND mean_visibility_miles <= ?", min, max).count
+    total_days == 0 ? 0 : (trips/total_days).round(2)
+  end
+
+  def self.max_trips_with_visibility(min, max)
+    trips = trips_with_visibility(min, max)
+    trips.group('start_date')
+    .order('count_all DESC')
+    .count
+    .values
+    .first
+  end
+
+  def self.min_trips_with_visibility(min, max)
+    trips = trips_with_visibility(min, max)
     trips.group('start_date')
     .order('count_all DESC')
     .count
