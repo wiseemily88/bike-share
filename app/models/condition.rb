426cbe4dab03e5945504727422d455374ee8d558
Luis Garcia <garcia@yahoo.com>
2017-10-11 12:04:45 -0600

    trip_array

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -14,4 +14,27 @@ class Condition < ActiveRecord::Base
   validates :precipitation_inches,presence: true
   validates :zip_code,            presence: true
 
+  def self.trip_array(range)
+    to_a.group_by do |condition|
+      condition.max_temperature_f
+    end.select do |k, v|
+      range.include?(k)
+    end.values.flatten.map do |condition|
+      condition.trips
+    end
+  end
+
+  def self.average_number_of_rides(trip_array)
+    (trip_array.sum) / (trip_array.count)
+  end
+
+  def self.highest_number_of_rides
+    
+  end
+
+  def self.lowest_number_of_rides
+    
+  end
+
+
 end
