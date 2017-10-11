43c16f41cdd695d7fe5bf67435d96fe5bb2236ae
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-11 13:01:42 -0600

    finished highest number of rides max-temps

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -5,15 +5,15 @@ class Condition < ActiveRecord::Base
 
   has_many :trips , class_name: "Trip", primary_key: "date", foreign_key: "start_date"
 
-  validates :date,                presence: true, uniqueness: true
-  validates :max_temperature_f,    presence: true
-  validates :min_temperature_f,   presence: true
-  validates :mean_temperature_f,  presence: true
-  validates :mean_humidity,       presence: true
-  validates :mean_visibility_miles,presence: true
-  validates :mean_wind_speed_mph,  presence: true
-  validates :precipitation_inches,presence: true
-  validates :zip_code,            presence: true
+  validates :date,                  presence: true, uniqueness: true
+  validates :max_temperature_f,     presence: true
+  validates :min_temperature_f,     presence: true
+  validates :mean_temperature_f,    presence: true
+  validates :mean_humidity,         presence: true
+  validates :mean_visibility_miles, presence: true
+  validates :mean_wind_speed_mph,   presence: true
+  validates :precipitation_inches,  presence: true
+  validates :zip_code,              presence: true
 
   def self.trip_array(range)
     all.group_by do |condition|
@@ -24,10 +24,6 @@ class Condition < ActiveRecord::Base
       condition.trips.count
     end.flatten
   end
-
-  # def self.average_number_of_rides(trip_array)
-  #   (trip_array.sum) / (trip_array.count)
-  # end
 
   def self.highest_number_of_rides
 
