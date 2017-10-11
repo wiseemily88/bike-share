02084730a26c800c5f7b4020fb83da1f7e56d9c9
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-11 10:01:10 -0600

    remove S from mean_wind_speed

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -7,7 +7,7 @@ class Condition < ActiveRecord::Base
   validates :mean_temperature_f,  presence: true
   validates :mean_humidity,       presence: true
   validates :mean_visibility_miles,presence: true
-  validates :mean_wind_Speed_mph,  presence: true
+  validates :mean_wind_speed_mph,  presence: true
   validates :precipitation_inches,presence: true
   validates :zip_code,            presence: true
 
