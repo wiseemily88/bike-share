056a3da14419e8f8dfa06b73b36dc3dc6c0f1736
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-11 19:19:46 -0600

    add .average_trips_with_wind_speed unit test

--- spec/models/conditions_spec.rb
+++ spec/models/conditions_spec.rb
@@ -170,7 +170,7 @@ describe Condition do
     # end
 
     describe ".trips_with_wind_speed"
-      it "returns collection of conditions between wind speed associated with trips" do
+      it "returns count of conditions between wind speed associated with trips" do
         Condition.create!(
           date: Date.new(2013,8,28),
           max_temperature_f: 40,
@@ -199,4 +199,46 @@ describe Condition do
         expect(Condition.all.trips_with_wind_speed(0, 4)).to eq(1)
     end
   end
-end
+
+  describe ".average_trips_with_wind_speed"
+    it "returns average wind speed associated with trips" do
+      Condition.create!(
+        date: Date.new(2013,8,28),
+        max_temperature_f: 40,
+        min_temperature_f: 60,
+        mean_temperature_f: 30,
+        mean_humidity: 10,
+        mean_visibility_miles: 4 ,
+        mean_wind_speed_mph: 1,
+        precipitation_inches: 4,
+        zip_code: 54401
+        )
+
+      Condition.create!(
+        date: Date.new(2013,8,28),
+        max_temperature_f: 40,
+        min_temperature_f: 60,
+        mean_temperature_f: 30,
+        mean_humidity: 10,
+        mean_visibility_miles: 4 ,
+        mean_wind_speed_mph: 3,
+        precipitation_inches: 4,
+        zip_code: 54401
+        )
+
+      Trip.create!(
+        duration: 63,
+        start_station_name: "South Van Ness at Market",
+        start_date: DateTime.new(2013,8,28,14,14),
+        start_station_id: 66,
+        end_date: DateTime.new(2013,8,28,14,14),
+        end_station_name: "South Van Ness at Market",
+        end_station_id: 66,
+        bike_id: 520,
+        subscription_type: "Subscriber",
+        zip_code: 94127
+        )
+
+      expect(Condition.all.trips_with_wind_speed(0, 4)).to eq(2)
+    end
+  end
